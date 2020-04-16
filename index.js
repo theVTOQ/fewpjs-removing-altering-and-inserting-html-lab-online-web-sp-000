939e3584e07ac9620277bb083370529cbf381c9c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-16 05:36:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+document.querySelector("#main").remove();
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "is the champion";
