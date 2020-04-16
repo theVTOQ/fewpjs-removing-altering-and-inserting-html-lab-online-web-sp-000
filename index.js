a5159285ec8593783b3f4fd6f99187945fa8868c
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-16 05:35:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.querySelector("#main").remove();
-let newHeader = document.createElement("h1#victory");
+let newHeader = document.createElement("h1");
 document.body.appendChild(newHeader);
 newHeader.innerHTML = "is the champion";
