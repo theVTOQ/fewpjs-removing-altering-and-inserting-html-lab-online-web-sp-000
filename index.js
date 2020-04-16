685f244624bba1f6a882e0990a75390ab8aec0fb
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2020-04-16 05:30:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Write your code here!\
-document.querySelector('main').remove
-let newHeader = document.createElement("h1#victory")
-document.body.appendChild(newHeader)
-newHeader.innerHTML = "is the champion"
+document.querySelector('main').remove;
+let newHeader = document.createElement("h1#victory");
+document.body.appendChild(newHeader);
+newHeader.innerHTML = "is the champion";
